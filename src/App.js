d4f43783f89e9efca676b1b05c99df83be7e5321
horro <faizalfachrozi@gmail.com>
2019-06-07 11:34:04 +0700

    yml

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi, thereassssas! 
+          Hi, thereassssasaa! 
         </p>
         <a
           className="App-link" 
